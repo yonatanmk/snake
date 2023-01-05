338364f49c00ad1983b83db120651acdbfce96e5
yonatanmk <yonatanmk@gmail.com>
2023-01-05 14:25:26 -0800

    initial grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import "./App.scss";
+
+const GRID_SIZE = 17;
+
+const CELL_TYPES = Object.freeze({
+  HEAD: "head",
+  TAIL: "tail",
+  EMPTY: "empty",
+  POINT: "point",
+});
+
+const DIRECTIONS = Object.freeze({
+  UP: "up",
+  DOWN: "down",
+  LEFT: "left",
+  RIGHT: "right",
+});
 
 function App() {
+  const [cells, setCells] = useState(
+    Array(GRID_SIZE * GRID_SIZE)
+      .fill(CELL_TYPES.EMPTY)
+      .fill(
+        CELL_TYPES.HEAD,
+        (GRID_SIZE * GRID_SIZE - 1) / 2,
+        (GRID_SIZE * GRID_SIZE - 1) / 2 + 1
+      )
+  );
+  const [direction, setDirection] = useState(DIRECTIONS.UP);
+  const timeInterval = useRef(500);
+
+  const increment = () => {
+    const currentIndex = cells.findIndex((cell) => cell === CELL_TYPES.HEAD);
+    const newCells = [...cells];
+    if (currentIndex === GRID_SIZE * GRID_SIZE - 1) {
+      newCells[0] = CELL_TYPES.HEAD;
+    } else {
+      newCells[currentIndex + 1] = CELL_TYPES.HEAD;
+    }
+    newCells[currentIndex] = CELL_TYPES.EMPTY;
+    setCells(newCells);
+  };
+
+  useEffect(() => {
+    console.log({
+      timeInterval,
+    });
+    const intervalId = setInterval(() => increment(), timeInterval.current);
+    return () => clearInterval(intervalId);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>SNAKE</h3>
+      <p>Score: 0</p>
+      <label>Time Interval</label>
+      <input
+        type="number"
+        value={timeInterval.current}
+        onChange={(e) => {
+          timeInterval.current = e.target.value;
+        }}
+      />
+      <div className="grid">
+        {cells.map((cell) => (
+          <div
+            className={["cell", cell === CELL_TYPES.HEAD && "cell--head"]
+              .filter(Boolean)
+              .join(" ")}
+          />
+        ))}
+      </div>
     </div>
   );
 }
